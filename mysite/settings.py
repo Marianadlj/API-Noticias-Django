58a149de7eefa3bc9489125e9ffa1d151d141ab7
Aldeander Santos <65050933+aldeandersantos@users.noreply.github.com>
2023-01-20 23:09:25 -0300

    :wrench: chore(gitignore) and refactor(delet virtualenv): Adição de gitignore file e correção de envio do ambiente virtual

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6nnzuw#1$2_7x8@celoc5g+oc1_)!3kt*(^vrd&iuj9e9_bk$c'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
